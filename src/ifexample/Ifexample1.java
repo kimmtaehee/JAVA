425f0c7997a25164ee3b2b1612464ce72e53715d
taehee <taehee@218.232.71.216>
2021-06-14 23:04:59 +0900

    21.06.14 if else

---
+++ src/ifexample/Ifexample1.java
@@ -0,0 +1,29 @@
+package ifexample;
+
+public class Ifexample1 {
+
+	public static void main(String[] args) {
+		
+		int age = 7;
+		if(age >= 8) {
+			System.out.println("학교에 다닙니다");
+		}
+		else {
+			System.out.println("학교에 다니지않습니다");
+		}
+
+		System.out.println("-----------------");
+		
+		char gender = 'F';
+		if(gender == 'F') {
+			System.out.println("여성입니다");
+		}
+		else {
+			System.out.println("남성입니다");
+		}
+		
+		
+		
+	}
+
+}
---
+++ src/ifexample/Ifexample3.java
@@ -0,0 +1,36 @@
+package ifexample;
+
+import java.util.Scanner;
+
+public class Ifexample3 {
+	public static void main(String[] args) {
+		
+		//예제
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("점수는 : ");
+		int score  = sc.nextInt();
+		
+		
+		if(90 <= score || score >= 100) {
+			System.out.println("A");
+		}
+		else if(80 <= score || score >= 89) {
+			System.out.println("B");
+		}
+		else if(70 <= score || score >= 79) {
+			System.out.println("C");
+		}
+		else if(60 <= score || score >= 69) {
+			System.out.println("D");
+		}
+		else {
+			System.out.println("F");
+		}
+		
+		sc.close();
+		
+		
+	}
+}
